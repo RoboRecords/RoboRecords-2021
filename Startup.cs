2da9634078eee2515c4e91a8a9e5ebab5bb74327
Leminn <47994143+Leminn@users.noreply.github.com>
2021-05-06 17:04:39 +0100

    Basic layout

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
