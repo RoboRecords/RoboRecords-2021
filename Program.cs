98d47611df06fa67755f8a39a9a0f2e07915832b
Refrag <R3FR4GYT@gmail.com>
2021-06-01 18:31:16 +0200

    Added the license to the source files
    And removed useless part of the license file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
+/*
+ * Program.cs: The website entry point
+ * Copyright (C) 2021, Lemin <Leminn> and Refrag <R3FR4G>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the 'LICENSE' file for more details.
+ */
+    
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,14 @@
+/*
+ * Startup.cs: the website main configuration
+ * Copyright (C) 2021, Lemin <Leminn> and Refrag <R3FR4G>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the 'LICENSE' file for more details.
+ */
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
