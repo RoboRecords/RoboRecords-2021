230687f04b9d9ed07eb316bcc9a1a8a7a1cf008b
Refrag <R3FR4GYT@gmail.com>
2021-06-05 17:44:12 +0200

    Updated the license in the files
    ...again :stuck_out_tongue:

--- Models/RoboUser.cs
+++ Models/RoboUser.cs
@@ -1,3 +1,14 @@
+/*
+ * RoboUser.cs: the user model as stored in the database
+ * Copyright (C) 2021, Refrag <R3FR4G>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the 'LICENSE' file for more details.
+ */
+
 using System;
 using AspNetCore.Identity.Mongo.Model;
 using MongoDB.Bson.Serialization.Attributes;
