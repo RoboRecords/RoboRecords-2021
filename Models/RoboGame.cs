2fbc8df391b6f8a9ff15a460eca8292f0a8d99b4
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2021-06-12 11:39:31 +0300

    Fix alignmrnt and errors

--- Models/RoboGame.cs
+++ Models/RoboGame.cs
@@ -1,17 +1,28 @@
-using System.Collections.Generic;;
+/*
+ * RoboLevel.cs
+ * Copyright (C) 2021, Ors <Riku-S>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the 'LICENSE' file for more details.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using AspNetCore.Identity.Mongo.Model;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace RoboRecords.Models
 {
     public class RoboGame
     {
         public string Name;
-        public string Icon;
+        public string IconPath;
         public List<RoboLevel> Levels;
 
-        public RoboGame()
-        {
-            Levels = new List<RoboLevel>();
-        }
         public RoboGame(string name)
         {
             Name = name;
--- Pages/Games.cshtml.cs
+++ Pages/Games.cshtml.cs
@@ -20,9 +20,9 @@ namespace RoboRecords.Pages
         {
             
             _games = new List<RoboGame>();
-            _games.Add(new RoboGame("Sonic Robo Blast 2 v2.2"){Icon = "../assets/images/gfz2bg.png", Levels = new List<RoboLevel>()});
-            _games.Add(new RoboGame("srb2 Cyberdime Realm"){Icon = "../assets/images/cydmbg.png", Levels = new List<RoboLevel>()});
-            _games.Add(new RoboGame("Sonic Robo Blast 3 LUL"){Icon = "../assets/images/dreamhill1.png", Levels = new List<RoboLevel>()});
+            _games.Add(new RoboGame("Sonic Robo Blast 2 v2.2"){IconPath = "../assets/images/gfz2bg.png", Levels = new List<RoboLevel>()});
+            _games.Add(new RoboGame("srb2 Cyberdime Realm"){IconPath = "../assets/images/cydmbg.png", Levels = new List<RoboLevel>()});
+            _games.Add(new RoboGame("Sonic Robo Blast 3 LUL"){IconPath = "../assets/images/dreamhill1.png", Levels = new List<RoboLevel>()});
             
             OrganizeGames();
         }
