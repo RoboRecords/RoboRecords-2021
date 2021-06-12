aceb036a9db687363f7ab16088565658ce92e278
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2021-06-12 11:22:57 +0300

    Add code for initializing maps

--- Pages/Games.cshtml.cs
+++ Pages/Games.cshtml.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RoboRecords.Models;
+using System.Collections.Generic;
 
 namespace RoboRecords.Pages
 {
     public class Games : PageModel
     {
+        private List<RoboGame> _games;
+        
+        private void OrganizeGames()
+        {
+            
+            foreach (var game in _games)
+            {
+                
+            }
+        }
         public void OnGet()
         {
             
+            _games = new List<RoboGame>();
+            _games.Add(new RoboGame("Sonic Robo Blast 2 v2.2"){Icon = "../assets/images/gfz2bg.png", Levels = new List<RoboLevel>()});
+            _games.Add(new RoboGame("srb2 Cyberdime Realm"){Icon = "../assets/images/cydmbg.png", Levels = new List<RoboLevel>()});
+            _games.Add(new RoboGame("Sonic Robo Blast 3 LUL"){Icon = "../assets/images/dreamhill1.png", Levels = new List<RoboLevel>()});
+            
+            OrganizeGames();
         }
     }
 }
