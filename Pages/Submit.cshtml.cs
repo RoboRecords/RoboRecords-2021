a2c4357814575e3342a718db23214990b62d8fe8
Ors <38985578+Riku-S@users.noreply.github.com>
2021-10-28 17:23:36 +0300

    Add a bare bones submit page with no functionalitien

---
+++ Pages/Submit.cshtml.cs
@@ -0,0 +1,25 @@
+/*
+ * Submit.cshtml.cs: the website main configuration
+ * Copyright (C) 2021, Ors <Riku-S>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * See the 'LICENSE' file for more details.
+ */
+
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RoboRecords.Pages
+{
+    public class Submit : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
