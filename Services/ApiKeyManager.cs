73478f92e9d913592a729cea24df9c19f4a4556c
Refrag <R3FR4GYT@gmail.com>
2021-11-29 18:08:05 +0100

    Implemented caching of the API keys hashes
    This greatly improves speed when using the web APIs a lot, this might need to be tested on a bigger scale though but it seemed to work fine in my testing
    This currently caches up to 20 keys, it is a bit of an arbitrary number but we don't want it to take too much memory either, we can adjust it later

--- Services/ApiKeyManager.cs
+++ Services/ApiKeyManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
@@ -14,12 +16,13 @@ namespace RoboRecords.Services
 
         private const int ApiKeyStringSize = 32;
         
-        //TODO: Cache some amount of API keys to avoid having to go through them all the time
+        private ApiKeyCache _apiKeyCache = new ApiKeyCache();
+        
         public bool TryAuthenticateFromApiKey(string apiKey, out RoboUser roboUser)
         {
             string hashedKey = HashApiKey(apiKey);
 
-            roboUser = DbSelector.GetRoboUserFromApiKey(hashedKey);
+            roboUser = GetRoboUser(hashedKey);
 
             return roboUser.UserNameNoDiscrim != "Invalid User";
         }
@@ -36,10 +39,18 @@ namespace RoboRecords.Services
 
             string apiKey = apiKeyBuilder.ToString();
 
-            user.ApiKey = HashApiKey(apiKey);
+            string oldApiKey = user.ApiKey;
+            string newApiKey = HashApiKey(apiKey);
+
+            user.ApiKey = newApiKey;
 
             DbUpdater.UpdateIdentityUser(user);
             
+            if (!string.IsNullOrEmpty(oldApiKey) && _apiKeyCache.ContainsKey(oldApiKey))
+                _apiKeyCache.UpdateKey(oldApiKey, newApiKey);
+            else
+                _apiKeyCache.AddKey(newApiKey, DbSelector.GetRoboUserFromApiKey(newApiKey));
+
             return apiKey;
         }
 
@@ -57,6 +68,67 @@ namespace RoboRecords.Services
             }
             
             return hash;
+        }
+
+        private RoboUser GetRoboUser(string apiKey)
+        {
+            if (_apiKeyCache.ContainsKey(apiKey))
+                return _apiKeyCache.GetRoboUser(apiKey);
+
+            RoboUser user = DbSelector.GetRoboUserFromApiKey(apiKey);
+            
+            if (user.UserNameNoDiscrim != "Invalid User")
+                _apiKeyCache.AddKey(apiKey, user);
+            
+            return user;
+        }
+    }
+
+    public class ApiKeyCache
+    {
+        private const int ApiKeyCacheCapacity = 20;
+
+        private OrderedDictionary _cache = new OrderedDictionary(ApiKeyCacheCapacity);
+        
+        public bool ContainsKey(string apiKey) => _cache.Contains(apiKey);
+
+        public RoboUser GetRoboUser(string apiKey)
+        {
+            RoboUser user = (RoboUser)_cache[apiKey];
+            
+            _cache.Remove(apiKey);
+            _cache.Add(apiKey, user);
+            return user;
+        }
+
+        public void AddKey(string apiKey, RoboUser user)
+        {
+            if (_cache.Keys.Count == ApiKeyCacheCapacity)
+                _cache.RemoveAt(0);
+
+            _cache.Add(apiKey, user);
+        }
+
+        public void UpdateKey(string oldApiKey, string newApiKey)
+        {
+            RoboUser user = (RoboUser)_cache[oldApiKey];
+            
+            _cache.Remove(oldApiKey);
+            _cache.Add(newApiKey, user);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (DictionaryEntry o in _cache)
+            {
+                RoboUser user = (RoboUser)o.Value;
+                
+                sb.AppendLine($"\t-{o.Key} : {user.UserNameNoDiscrim}#{user.Discriminator}");
+            }
+
+            return sb.ToString();
         }
     }
 }
