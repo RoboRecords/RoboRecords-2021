4b71894e434e006076efbbc016b6e6198c8cec5e
Refrag <R3FR4GYT@gmail.com>
2022-02-18 19:28:22 +0100

    RoboPageModel: Lazy initialization of RoboUser and IdentityRoboUser
    This improves performance because of not having to get the user multiple times (yes that was happening before) and not having to get the IdentityRoboUser if not needed

--- RoboRecords/Models/RoboPageModel.cs
+++ RoboRecords/Models/RoboPageModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,42 +9,58 @@ namespace RoboRecords.Models
     public class RoboPageModel : PageModel
     {
         private IHttpContextAccessor _httpContextAccessor;
+
+        private bool _initialized = false;
         
         public RoboPageModel(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
 
-            // Trigger the getter to set _foundUserLastResult
-            _ = CurrentUser;
-            _ = CurrentIdentityUser;
+            _currentUser = new Lazy<RoboUser>(GetUserAndUpdateResult);
+            _currentIdentityUser = new Lazy<IdentityRoboUser>(GetIdentityUserAndUpdateResult);
         }
         
-        public bool IsLoggedIn => _foundUserLastResult;
+        public bool IsLoggedIn
+        {
+            get
+            {
+                if (_currentIdentityUser.IsValueCreated)
+                    return _foundUserLastResult;
+                else
+                {
+                    GetUserAndUpdateResult();
+                    return _foundUserLastResult;
+                }
+            }
+        }
 
         public bool isModerator = false;
 
-        public RoboUser CurrentUser
-        {
-            get
-            {
-                RoboUser roboUser;
-                _foundUserLastResult = DbSelector.TryGetRoboUserFromUserName(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name), out roboUser);
-                return roboUser;
-            }
-        }
+        private Lazy<RoboUser> _currentUser;
         
+        public RoboUser CurrentUser => _currentUser.Value;
+
         private bool _foundUserLastResult;
         
-        public IdentityRoboUser CurrentIdentityUser
+
+        private Lazy<IdentityRoboUser> _currentIdentityUser;
+        
+        public IdentityRoboUser CurrentIdentityUser => _currentIdentityUser.Value;
+
+        private bool _foundIdentityUserLastResult;
+
+        private RoboUser GetUserAndUpdateResult()
         {
-            get
-            {
-                IdentityRoboUser identityRoboUser;
-                _foundIdentityUserLastResult = DbSelector.TryGetIdentityUserFromUserName(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name), out identityRoboUser);
-                return identityRoboUser;
-            }
+            RoboUser roboUser;
+            _foundUserLastResult = DbSelector.TryGetRoboUserFromUserName(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name), out roboUser);
+            return roboUser;
         }
         
-        private bool _foundIdentityUserLastResult;
+        private IdentityRoboUser GetIdentityUserAndUpdateResult()
+        {
+            IdentityRoboUser identityRoboUser;
+            _foundIdentityUserLastResult = DbSelector.TryGetIdentityUserFromUserName(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name), out identityRoboUser);
+            return identityRoboUser;
+        }
     }
 }
