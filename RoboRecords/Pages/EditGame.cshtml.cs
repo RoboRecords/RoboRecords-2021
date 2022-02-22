2fbcab888b98c09533155f758cb02c708a0c12c6
Ors <38985578+Riku-S@users.noreply.github.com>
2022-02-23 00:36:36 +0200

    Make the game edit support nights and post json stuff, and parse it

--- RoboRecords/Pages/EditGame.cshtml.cs
+++ RoboRecords/Pages/EditGame.cshtml.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Web;
+using Microsoft.AspNetCore.Antiforgery;
 using RoboRecords.DbInteraction;
 using RoboRecords.Models;
 
@@ -13,13 +16,17 @@ namespace RoboRecords.Pages
         [BindProperty]
         public GameEditDb GameEdit { get; set; }
         public static RoboGame Game;
-
-        public EditGame(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        public string Token;
+        
+        private IAntiforgery _antiforgery;
+        public EditGame(IAntiforgery antiforgery, IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
         {
+            _antiforgery = antiforgery;
         }
         
         public void OnGet()
         {
+            Token = _antiforgery.GetTokens(HttpContext).RequestToken;
             DbSelector.TryGetGamesWithLevels(out var roboGames);
             // Game = roboGames[1]; // DONE: CHANGE THIS HARDCODED CRAP TO SUPPORT THE CURRENT SELECTED GAME --- Zenya
 
@@ -34,6 +41,41 @@ namespace RoboRecords.Pages
                 // SRB2 2.2 default for testing. Should be changed to throw an error.
                 DbSelector.TryGetGameWithLevelsFromID("sonicroboblast2v22", out Game);
             }
+        }
+        public class GameData
+        {
+            public string Name { get; set; }
+            public string UrlName { get; set; }
+            public string Groups { get; set; }
+        }
+
+        class InterpretedLevel
+        {
+            public string levelName;
+            public int act;
+            public int number;
+            public bool nights;
+            public string iconPath;
+        }
+        
+        class InterpretedGroup
+        {
+            public string groupName{ get; set; }
+            public bool writeNames{ get; set; }
+            public InterpretedLevel[] levels{ get; set; }
+        }
+        
+        public IActionResult OnPostSaveAsync([FromBody] GameData data)
+        {
+            Logger.Log(data.Groups, true);
+            List<InterpretedGroup> groups =
+                System.Text.Json.JsonSerializer.Deserialize<List<InterpretedGroup>>(data.Groups);
+            
+            Logger.Log("Length" + groups.Count, true);
+            
+            // TODO: Compare the data to existing data, and replace what needs to be replaced in order to edit the game
+            
+            return Page();
         }
     }
 }
