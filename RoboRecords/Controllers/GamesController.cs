64f905883b73e409527e33a6f08579622dc20e03
Refrag <R3FR4GYT@gmail.com>
2022-02-23 21:13:42 +0100

    API Controllers: Add a Game controller and improve upon the Runs controller
    This introduces a new API controller that will retrieve all games or a specific game by giving it the DbID.
    The runs controller's submit method will now report to the user the errors that happened and will actually save the replay to the website, similarly to how the submit page works

---
+++ RoboRecords/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using RoboRecords.DbInteraction;
+using RoboRecords.Models;
+
+namespace RoboRecords.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetGameFromId(int id)
+        {
+            if (!DbSelector.TryGetGameWithLevelsFromDbID(id, out RoboGame roboGame))
+                return NotFound("No game found at that address");
+            return Ok(roboGame);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetGames()
+        {
+            if (!DbSelector.TryGetGamesWithLevels(out List<RoboGame> roboGames))
+                return Problem();
+            return Ok(roboGames);
+        }
+    }
+}
--- RoboRecords/Controllers/RunsController.cs
+++ RoboRecords/Controllers/RunsController.cs
@@ -2,6 +2,7 @@ using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using RoboRecords.DbInteraction;
 using RoboRecords.Filters;
 using RoboRecords.Models;
 
@@ -11,17 +12,19 @@ namespace RoboRecords.Controllers
     [ApiController]
     public class RunsController : ControllerBase
     {
-        //TODO: 
-        //   - Get the user from the request somehow
-        //   - Save the replay
-        //   - Give more information about what failed if it failed
         [RequireApiKey]
         [HttpPost]
         [Route("submit")]
-        public IActionResult Submit([FromQuery(Name = "File")] IFormFile file)
+        public IActionResult Submit([FromQuery] int gameId, [FromQuery(Name = "File")] IFormFile file)
         {
+            if (gameId == 0)
+                return BadRequest("Game ID was not provided");
+
             if (file is null)
-                return NoContent();
+                return BadRequest("Replay file was not provided");
+            
+            if (!DbSelector.TryGetGameFromDbID(gameId, out RoboGame roboGame))
+                return BadRequest($"The game with the id {gameId} was not found");
 
             byte[] fileBytes = new byte[file.Length];
             
@@ -29,8 +32,22 @@ namespace RoboRecords.Controllers
                 fileStream.Read(fileBytes, 0, (int)file.Length);
 
             RoboRecord record = new RoboRecord((RoboUser)RouteData.Values["apiKeyRoboUser"], fileBytes);
-            if (record.FileBytes == null)
-                return UnprocessableEntity();
+            switch (record.readStatus)
+            {
+                case RoboRecord.ReadStatus.Success:
+                    // The condition SHOULD always be true, but let's be careful
+                    if (record.FileBytes != null && record.FileBytes.Length > 0)
+                        break;
+                    else
+                        return Problem($"Unknown error when processing {file.Name}: {RoboRecord.MessageSuccess}");
+                default:
+                    return UnprocessableEntity(
+                        $"Error when processing {file.Name}: {RoboRecord.GetReadStatusMessage(record.readStatus)}");
+            }
+            
+            DbSelector.TryGetGameLevelFromMapId(roboGame.UrlName, record.LevelNumber.ToString(), out RoboLevel level);
+            if (!DbInserter.AddRecordIfNeeded(record, level))
+                return Conflict("The same record or a better one was already submitted");
             
             return Ok(record);
         }
